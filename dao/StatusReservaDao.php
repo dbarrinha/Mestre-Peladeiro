bf81bf0aed5ffb1ac89c510d791b97b364336e9d
Computacao2014 <cc2014.1@outlook.com>
2018-05-24 20:32:05 -0300

    daos danilo finalizados

--- dao/StatusReservaDao.php
+++ dao/StatusReservaDao.php
@@ -1,5 +1,23 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/dao/ConexaoDao.php';
+class StatusReservaDao {
+    
+    public function getStatusReservaById($id){
+      $con = ConexaoDao::getConexao();
+      $query = "SELECT * FROM status_reserva WHERE id=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("i", $id);
+      if($stmt->execute()===TRUE){
+         $result = $stmt->get_result();
+         $arrayDiaSemana = $result->fetch_assoc();
+         $stmt->close();
+         $con->close();
+         return $arrayDiaSemana;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
 
-
-
+}
 
--- dao/TipoCampoDao.php
+++ dao/TipoCampoDao.php
@@ -1,2 +1,23 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/dao/ConexaoDao.php';
+class TipoCampoDao {
+    
+    public function getTipoCampoById($id){
+      $con = ConexaoDao::getConexao();
+      $query = "SELECT * FROM tipo_campo WHERE id=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("i", $id);
+      if($stmt->execute()===TRUE){
+         $result = $stmt->get_result();
+         $arrayDiaSemana = $result->fetch_assoc();
+         $stmt->close();
+         $con->close();
+         return $arrayDiaSemana;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
+
+}
 
