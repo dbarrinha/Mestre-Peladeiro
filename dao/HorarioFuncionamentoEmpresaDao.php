fdd242b2563c9e56ae0ca3e43d4a73b364b1a2a7
danilo.barrinha <danilo.barrinha@DISAU-07122.tce.pi.gov.br>
2018-05-25 08:22:46 -0300

    daos finalizados

--- dao/HorarioFuncionamentoEmpresaDao.php
+++ dao/HorarioFuncionamentoEmpresaDao.php
@@ -1,3 +1,74 @@
 <?php
 
+class HorarioFuncionamentoEmpresaDao{
+    
+    public function atualizarHorario($horario){
+      $con = ConexaoDao::getConexao();
+      $query = "UPDATE horario_funcion_empresa SET id_empresa=?, id_dia_semana=?, horario_inicio=?"
+              . ", horario_fim=? WHERE id=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("iissi", $horario['id_empresa'], $horario['id_dia_semana'], $horario['horario_inicio'], 
+            $horario['horario_fim'], $horario['id']);
+      if($stmt->execute()===TRUE){
+         $stmt->close();
+         $con->close();
+         return TRUE;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
+   
+   public function inserirHorario($horario){
+      $con = ConexaoDao::getConexao();
+      $query = "INSERT INTO horario_funcion_empresa VALUES (NULL,?,?,?,?)";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("iiss", $horario['id_empresa'], $horario['id_dia_semana'], $horario['horario_inicio'], 
+            $horario['horario_fim']);
+      if($stmt->execute()===TRUE){
+         $stmt->close();
+         $con->close();
+         return TRUE;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
+  
+   
+   public function getHorarioById($id){
+      $con = ConexaoDao::getConexao();
+      $query = "SELECT * FROM horario_funcion_empresa WHERE id=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("i", $id);
+      if($stmt->execute()===TRUE){
+         $result = $stmt->get_result();
+         $arrayCampo = $result->fetch_assoc();
+         $stmt->close();
+         $con->close();
+         return $arrayCampo;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
+   
+   public function getHorarioByEmpresa($idEmpresa){
+      $con = ConexaoDao::getConexao();
+      $query = "SELECT * FROM horario_funcion_empresa WHERE id_empresa=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("i", $idEmpresa);
+      if($stmt->execute()===TRUE){
+         $result = $stmt->get_result();
+         $arrayCampo = $result->fetch_all(MYSQLI_ASSOC);
+         $stmt->close();
+         $con->close();
+         return $arrayCampo;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }  
+}
+
 
--- dao/ReservaDao.php
+++ dao/ReservaDao.php
@@ -1,87 +1,73 @@
 <?php
 
-class Reserva {
-    private $id;
-    private $idStatusReserva;
-    private $idUsuario;
-    private $idEmpresa;
-    private $idCampo;
-    private $dataInicio;
-    private $dataFim;
-    private $taxaPreReserva;
-    private $taxaReserva;
+class ReservaDao {
     
-    function getId() {
-        return $this->id;
-    }
-
-    function getIdStatusReserva() {
-        return $this->idStatusReserva;
-    }
-
-    function getIdUsuario() {
-        return $this->idUsuario;
-    }
-
-    function getIdEmpresa() {
-        return $this->idEmpresa;
-    }
-
-    function getIdCampo() {
-        return $this->idCampo;
-    }
-
-    function getDataInicio() {
-        return $this->dataInicio;
-    }
-
-    function getDataFim() {
-        return $this->dataFim;
-    }
-
-    function getTaxaPreReserva() {
-        return $this->taxaPreReserva;
-    }
-
-    function getTaxaReserva() {
-        return $this->taxaReserva;
-    }
-
-    function setId($id) {
-        $this->id = $id;
-    }
-
-    function setIdStatusReserva($idStatusReserva) {
-        $this->idStatusReserva = $idStatusReserva;
-    }
-
-    function setIdUsuario($idUsuario) {
-        $this->idUsuario = $idUsuario;
-    }
-
-    function setIdEmpresa($idEmpresa) {
-        $this->idEmpresa = $idEmpresa;
-    }
-
-    function setIdCampo($idCampo) {
-        $this->idCampo = $idCampo;
-    }
-
-    function setDataInicio($dataInicio) {
-        $this->dataInicio = $dataInicio;
-    }
-
-    function setDataFim($dataFim) {
-        $this->dataFim = $dataFim;
-    }
-
-    function setTaxaPreReserva($taxaPreReserva) {
-        $this->taxaPreReserva = $taxaPreReserva;
-    }
-
-    function setTaxaReserva($taxaReserva) {
-        $this->taxaReserva = $taxaReserva;
-    }
-
+    public function atualizarReserva($reserva){
+      $con = ConexaoDao::getConexao();
+      $query = "UPDATE reserva SET id_status_reserva=?, id_usuario=?, id_empresa=?, id_campo=?, data_inicio=?"
+              . ", data_fim=?, taxa_pre_reserva=?, taxa_reserva=? WHERE id=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("iiiissddi", $reserva['id_status_reserva'], $reserva['id_usuario'], $reserva['id_empresa'], 
+            $reserva['id_campo'],$reserva['data_inicio'], $reserva['data_fim'],  $reserva['taxa_pre_reserva'], $reserva['taxa_reserva'], $reserva['id']);
+      if($stmt->execute()===TRUE){
+         $stmt->close();
+         $con->close();
+         return TRUE;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
+   
+   public function inserirReserva($reserva){
+      $con = ConexaoDao::getConexao();
+      $query = "INSERT INTO reserva VALUES (NULL,?,?,?,?,?,?,?,?)";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("iiiissdd", $reserva['id_status_reserva'], $reserva['id_usuario'], $reserva['id_empresa'], 
+            $reserva['id_campo'],$reserva['data_inicio'], $reserva['data_fim'],  $reserva['taxa_pre_reserva'], $reserva['taxa_reserva']);
+      if($stmt->execute()===TRUE){
+         $stmt->close();
+         $con->close();
+         return TRUE;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
+  
+   
+   public function getReservaById($id){
+      $con = ConexaoDao::getConexao();
+      $query = "SELECT * FROM reserva WHERE id=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("i", $id);
+      if($stmt->execute()===TRUE){
+         $result = $stmt->get_result();
+         $arrayCampo = $result->fetch_assoc();
+         $stmt->close();
+         $con->close();
+         return $arrayCampo;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }
+   
+   public function getReservaByUsuario($idUsuario){
+      $con = ConexaoDao::getConexao();
+      $query = "SELECT * FROM reserva WHERE id_usuario=?";
+      $stmt = $con->prepare($query);
+      $stmt->bind_param("i", $idUsuario);
+      if($stmt->execute()===TRUE){
+         $result = $stmt->get_result();
+         $arrayCampo = $result->fetch_all(MYSQLI_ASSOC);
+         $stmt->close();
+         $con->close();
+         return $arrayCampo;
+      }else{
+         $erro = $stmt->errno.' - '.$stmt->error;
+         return $erro;
+      }
+   }  
 
 }
